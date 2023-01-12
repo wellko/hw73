568de1a3e056301009219cfdd14c3f2b62459e15
Kwon Valerii <wellko24@gmail.com>
2023-01-12 17:16:40 +0600

    created response Echo function on server with 8000 port

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.get('/:text', (req, res) => {
+	res.set('Content-Type', 'text/plain');
+	res.send(req.params.text);
+});
+
+app.listen(port, () => {
+	console.log('We are live on ' + port);
+});
+
+app.get('/decode/:cipher', (req, res) => {
+	res.send()
+})
