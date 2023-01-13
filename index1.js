732ec03186fbeb54c9fa5655b95d78dde54bd7cf
Kwon Valerii <wellko24@gmail.com>
2023-01-13 13:40:20 +0600

    refactored code

--- index1.js
+++ index1.js
@@ -1,12 +1,10 @@
 const express = require('express');
-
 const app = express();
-
 const port = 8000;
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Echo go to /*anytext* for get response')
-})
+});
 
 app.get('/:text', (req, res) => {
 	res.set('Content-Type', 'text/plain');
--- index2.js
+++ index2.js
@@ -1,16 +1,10 @@
 const express = require('express');
-
 const Vigenere = require('caesar-salad').Vigenere;
-
 const app = express();
-
 const port = 8000;
-
 const password = 'well';
-
 const text = '<h1>Welcome to Cipher</h1> <p>go to /encode/<i>*sometext*</i> for cipher <i>*sometext*</i>' +
 	' <br> or /decode/<i>*cipher*</i> for decipher <i>*cipher*</i></p>'
-
 
 app.get('/', (req, res) => {
 	res.send(text);
@@ -23,7 +17,6 @@ app.get('/encode/:text', (req, res) => {
 app.get('/decode/:cipher', (req, res) => {
 	res.send(Vigenere.Decipher(password).crypt(req.params.cipher));
 });
-
 
 app.listen(port, () => {
 	console.log('We are live on ' + port);
